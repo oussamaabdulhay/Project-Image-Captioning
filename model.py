1e0b43484ac3203bf2f14e18703a77067c95d32c
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-06-25 17:17:26 +0400

    uploading the files

--- model.py
+++ model.py
@@ -20,14 +20,58 @@ class EncoderCNN(nn.Module):
         features = self.embed(features)
         return features
     
-
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+        
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(embed_size, hidden_size)
+        
+        self.hidden2tag = nn.Linear(hidden_size, vocab_size)
+        
+       
     
     def forward(self, features, captions):
-        pass
-
+        
+        captions = captions[:, :-1] 
+        
+        captions_embeded = self.word_embeddings(captions)
+        
+             
+        concatenate= torch.cat((features.unsqueeze(1), captions_embeded), dim=1)
+        
+        lstm_out,_ = self.lstm(concatenate)
+        
+        output = self.hidden2tag(lstm_out)
+        
+        return output
+    
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        counter = 0
+        final_output_list = [];
+        print(inputs.shape)
+        while (counter < max_len):
+            lstm_out, _ = self.lstm(inputs)
+
+            output = self.hidden2tag(lstm_out)
+            temp = output.squeeze(0)
+            temp = temp.squeeze(0)
+            output_list = [ int(x) for x in temp]
+            predicted_value = max(output_list)
+            final_output_list.append(predicted_value)
+            inputs = None
+            inputs = [1,1, predicted_value]
+            
+            inputs = torch.FloatTensor(inputs)
+            inputs = torch.tensor(inputs).to(torch.int64)
+            inputs = self.word_embeddings(inputs)
+            counter +=1
+        print(final_output_list)
+        return final_output_list
